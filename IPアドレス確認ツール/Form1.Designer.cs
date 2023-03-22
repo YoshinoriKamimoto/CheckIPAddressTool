7b023bde5df3eafa46d1abda9d3ccf5d7985f3d5
YoshinoriKamimoto <k.yoshinori0210@gmail.com>
2023-03-22 13:24:26 +0900

    IPアドレス確認ツール

---
+++ IPアドレス確認ツール/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace IPアドレス確認ツール
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.HostNameTextBox = new System.Windows.Forms.TextBox();
+            this.IPv4TextBox = new System.Windows.Forms.TextBox();
+            this.IPv6TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(126, 48);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(91, 38);
+            this.SearchButton.TabIndex = 0;
+            this.SearchButton.Text = "IPアドレス検索";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // HostNameTextBox
+            // 
+            this.HostNameTextBox.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.HostNameTextBox.Location = new System.Drawing.Point(59, 136);
+            this.HostNameTextBox.Name = "HostNameTextBox";
+            this.HostNameTextBox.ReadOnly = true;
+            this.HostNameTextBox.Size = new System.Drawing.Size(228, 22);
+            this.HostNameTextBox.TabIndex = 1;
+            // 
+            // IPv4TextBox
+            // 
+            this.IPv4TextBox.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.IPv4TextBox.Location = new System.Drawing.Point(59, 201);
+            this.IPv4TextBox.Name = "IPv4TextBox";
+            this.IPv4TextBox.ReadOnly = true;
+            this.IPv4TextBox.Size = new System.Drawing.Size(228, 22);
+            this.IPv4TextBox.TabIndex = 2;
+            // 
+            // IPv6TextBox
+            // 
+            this.IPv6TextBox.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.IPv6TextBox.Location = new System.Drawing.Point(59, 263);
+            this.IPv6TextBox.Name = "IPv6TextBox";
+            this.IPv6TextBox.ReadOnly = true;
+            this.IPv6TextBox.Size = new System.Drawing.Size(228, 22);
+            this.IPv6TextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(56, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ホスト名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(56, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IPv4アドレス";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(56, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "IPv6アドレス";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 328);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IPv6TextBox);
+            this.Controls.Add(this.IPv4TextBox);
+            this.Controls.Add(this.HostNameTextBox);
+            this.Controls.Add(this.SearchButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IPアドレス確認ツール";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.TextBox HostNameTextBox;
+        private System.Windows.Forms.TextBox IPv4TextBox;
+        private System.Windows.Forms.TextBox IPv6TextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ IPアドレス確認ツール/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPアドレス確認ツール
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            // コントロールの値をクリア
+            HostNameTextBox.Clear();
+            IPv4TextBox.Clear();
+            IPv6TextBox.Clear();
+
+            // IPアドレスを取得
+            string hostName = "";
+            string ipv4 = "";
+            string ipv6 = "";
+            try
+            {
+                hostName = Dns.GetHostName();
+                IPAddress[] ips = Dns.GetHostAddresses(hostName);
+                foreach (IPAddress ip in ips)
+                {
+                    // IPv4アドレスを取得
+                    if (ip.AddressFamily.Equals(AddressFamily.InterNetwork))
+                    {
+                        ipv4 = ip.ToString();
+                    }
+
+                    // IPv6アドレスを取得
+                    if (ip.AddressFamily.Equals(AddressFamily.InterNetworkV6))
+                    {
+                        ipv6 = ip.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"IPアドレス取得中にエラー発生\n{ex}");
+                MessageBox.Show($"IPアドレスの取得中にエラーが発生しました。\n\n{ex}",
+                                "エラー",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+
+
+            // 結果を表示
+            Console.WriteLine("IPアドレス取得完了");
+            Console.WriteLine($"ホスト名：{hostName}");
+            Console.WriteLine($"IPv4アドレス：{ipv4}");
+            Console.WriteLine($"IPv6アドレス：{ipv6}");
+            HostNameTextBox.Text = hostName;
+            IPv4TextBox.Text = ipv4;
+            IPv6TextBox.Text = ipv6;
+            MessageBox.Show("IPアドレス取得完了",
+                            "完了",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+    }
+}
